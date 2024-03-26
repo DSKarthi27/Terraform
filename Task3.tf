519fd5431162edfe3a5236ffe8b895c7f4eff778
Karthi D S <113298624+DSKarthi27@users.noreply.github.com>
2024-03-26 11:46:15 +0530

    EC2user with SG and Userdata

---
+++ Task3.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "***************"
+  secret_key = "*************************"
+}
+
+resource "aws_security_group" "web_node" {
+  name        = "web_node"
+  description = "Allow all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example-1" {
+  ami           = "ami-0187337106779cdf8"
+  instance_type = "t2.micro"
+  key_name      = "windows"
+  count         = "2"
+  tags          ={
+  Name="Karthi"
+}
+  security_groups = ["${aws_security_group.web_node.name}"]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+}
